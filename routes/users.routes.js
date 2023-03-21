22d0980020b176ecebaa719a6221777dc471803f
karen okasaki <oks.karen@gmail.com>
2023-03-21 11:53:39 -0300

    removing regex from password

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -16,12 +16,7 @@ router.post("/sign-up", async (req, res) => {
   try {
     const { password } = req.body;
 
-    if (
-      !password ||
-      !password.match(
-        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#_!])[0-9a-zA-Z$*&@#_!]{8,}$/
-      )
-    ) {
+    if (!password) {
       // retorna Bad Request
       return res.status(400).json({
         msg: "Password is required and must have at least 8 characters, uppercase and lowercase letters, numbers and special characters.",
